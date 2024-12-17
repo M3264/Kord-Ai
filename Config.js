ae0c838dd262784721a78ac0a86f239336a926ea
M3264 <Miracle32669@outlook.com>
2024-12-17 19:59:48 +0100

    V1.5.1

--- Config.js
+++ Config.js
@@ -1,32 +1,40 @@
+// hey there, kindly read this file till the end for better configuration
+
 global.settings = {
-    OWNER_NUMBERS: "2347013159244", // replace with your number
-    OWNER_NAME: 'Miracle', // desired name
-    BOT_NAME: "KORD", // desired name
-    VERSION: 'V1.0.0',
-    SESSION_ID: '', //put your session id between ''
-    MONGO_DB_URL: 'mongodb+srv://miracle32669:Iyanu1234@cluster0.m7glm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    WORK_MODE: "Public",
-    TIME_ZONE: "Africa/Lagos",
-    ANTI_VIEWONCE: true,
-    AUTO_READ_STATUS: true,
-    ALWAYS_ONLINE: true,
-    READ_ALL_MESSAGES: false,
-    PREFIX: [".", "/", "!"],
-    WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
-    DEFAULT_TRANSLATION_LANG: "en",
-    COMMAND_COOLDOWN_TIME_IN_MS: 2000,
-    MAX_COMMANDS_PER_MINUTE: 10,
-    MAX_DOWNLOAD_SIZE: 100,
-    API_KEYS: {
-        OPENWEATHERMAP_API_KEY: ''
-    },
-    isHackEnable:true,
-    SAFE_SEARCH: true,
-    REJECT_CALLS: true,
-    SEND_WELCOME_MESSAGE: false,
-    WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
-    DELETE_WA_LINKS: false,
-    BAD_WORD_FILTER: true,
-    BAD_WORDS: ['badword1', 'badword2', 'fuck', 'bitch', 'hentai'],
-    ANTI_DELETE_ENABLED: true
+  OWNER_NUMBERS: "2349067339193", // your number with country code
+  OWNER_NAME: 'Miracle', // your owner name 
+  BOT_NAME: "KORD", // your preferred bot name
+  SESSION_ID: '', // put your session id between the ''
+  INVINCIBLE_MODE: false, // toogle invincible mode 
+  WORK_MODE: "Public",
+  TIME_ZONE: "Africa/Lagos",
+  ANTI_VIEWONCE: true,
+  READ_ALL_MESSAGES: false,
+  AUTO_READ_STATUS: true,
+  PREFIX: [".", "/", "!"],
+  WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
+  DEFAULT_TRANSLATION_LANG: "en",
+  COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
+  MAX_COMMANDS_PER_MINUTE: 100,
+  MAX_DOWNLOAD_SIZE: 100,
+  AUTO_LIKE_STATUS: true,
+  AUTO_LIKE_EMOJI: '🍁',
+  BANKING_DETAILS: {
+    bank: "Kord-Ai Bank", // the name of your preferred bank
+    acc_number: "1234567890", // account number for your preferred bank
+    acc_name: "MyAccountName" // name of your bank
+  },
+  /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
+  API_KEYS: {
+    OPENWEATHERMAP_API_KEY: ''
+  },
+  isHackEnable: true,
+  SAFE_SEARCH: true,
+  REJECT_CALLS: true,
+  SEND_WELCOME_MESSAGE: false,
+  WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
+  DELETE_WA_LINKS: false,
+  BAD_WORD_FILTER: true,
+  BAD_WORDS: ['badword1', 'badword2', 'fuck', 'bitch', 'hentai'],
+  ANTI_DELETE_ENABLED: true,
 };
--- README.md
+++ README.md
@@ -30,7 +30,7 @@
   <a href="https://github.com/M3264/Kord-Ai/fork"><img title="KORD-AI" src="https://img.shields.io/badge/FORK-KORD AI-h?color=blue&style=for-the-badge&logo=stackshare"></a>
 
 
- ### Version 1.4.5
+ ### Version 1.5.1
 
 
 ## How To Deploy?
