30c12a9e65ffd1f04218b01bbcc35657ff82b634
Josh <realchelseajames80@gmail.com>
2025-03-04 13:05:58 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "1234564789", // your number with country code
-  OWNER_NAME: 'シ', // your owner name 
+  OWNER_NUMBERS: "2348034527722", // your number with country code
+  OWNER_NAME: 'JOSH', // your owner name 
   BOT_NAME: 'Kord AI', // your preferred bot name
-  SESSION_ID: '', // replace your_sessionid with yours and make sure its inside the 
+  SESSION_ID: 'kord_ai-uwcEXhawOWxJ', // replace your_sessionid with yours and make sure its inside the 
   INVINCIBLE_MODE: true, // toogle invincible mode 
   WORK_MODE: 'public',
   TIME_ZONE: "Africa/Lagos",
