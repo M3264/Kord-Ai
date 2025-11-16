992385d5eaac1c4384f91f53b48c5d2f21f552e2
Kenny <miracle32669@outlook.com>
2025-11-16 18:09:35 +0100

    ss.fix

--- cmds/utility.js
+++ cmds/utility.js
@@ -36,7 +36,7 @@ kord({
         var links = await extractUrlsFromString(lik)
         var link = links[0]
 
-        var img = await fetch(`https://api-rebix.zone.id/api/ssweb?url=${encodeURIComponent(link)}&device=pc`)
+        var img = await fetch(`https://api-rebix.zone.id/api/ssweb?url=${encodeURIComponent(link)}&device=desktop`)
         var imgbuff = await img.buffer()
         return m.send(imgbuff, { caption: "> here’s your screenshot", quoted: m }, "image")
     } catch (err) {
