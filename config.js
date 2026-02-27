6899f05eefb639442c03289901f8863abb908b54
bytedrex15-beep <bytedrex15@gmail.com>
2026-02-27 18:29:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = { 
-    SESSION_ID: process.env.SESSION_ID || '',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349067339193",
+    SESSION_ID: process.env.SESSION_ID || 'kord_ai-7LjomZMPAlI8wkof',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "255781250501",
     WORKTYPE: process.env.WORKTYPE || "private",
     PREFIX: process.env.PREFIX || "[.]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
@@ -12,7 +12,7 @@ module.exports = {
     STATUS_VIEW: process.env.STATUS_VIEW || true,
     SAVE_STATUS: process.env.SAVE_STATUS || false,
     LIKE_STATUS: process.env.SAVE_STATUS || false,
-    STATUS_EMOJI: process.env.STATUS_EMOJI || "🥏",
+    STATUS_EMOJI: process.env.STATUS_EMOJI || "🥇",
     ERROR_MSG: process.env.ERROR_MSG || true,
     TIMEZONE: process.env.TIMEZONE || "Africa/Lagos",
     CAPTION: process.env.CAPTION || "",
